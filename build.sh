b5cf1681b89c006048464efaed94331a7263ef46
Davide Rossetto <davide@crispybacon.it>
2018-05-20 15:27:05 +0200

    Fix server

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ DATE=$(date +"%Y-%m-%d_%H_%M")
 rm -rf dist
 npm run build
 tar czf scambialibri-frontend-$DATE.tar.gz dist/
-scp scambialibri-frontend-$DATE.tar.gz root@iamdavi.de:~/projects/scambialibri-frontend/releases
+scp scambialibri-frontend-$DATE.tar.gz root@iamdavi.de:~/projects/scambialibri-frontend/releases/scambialibri-frontend-$DATE.tar.gz
 ssh root@iamdavi.de -t "cd ~/projects/scambialibri-frontend/; rm -rf dist; tar xzf ./releases/scambialibri-frontend-$DATE.tar.gz"
 rm -f scambialibri-frontend-$DATE.tar.gz
